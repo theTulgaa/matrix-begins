9b12a97785ce5ded490b6dce7efa84e81e21a372
Batman <tulgalhagva04@gmail.com>
2024-04-15 08:44:00 +0800

    final

--- src/App.jsx
+++ src/App.jsx
@@ -152,14 +152,20 @@ function App() {
     
     let rank = 0;
     for (let i = 0; i < n; i++) {
-        if (matrix[i][m - 1] !== 0) {
-            rank++;
+      var checker = false;
+      for(let j = 0;j < m; j++){
+        if (matrix[i][j] !== 0) {
+          checker = true
         }
+      }
+      if(checker === true){
+        rank++;
+      }
+        
     }
     
     return rank;
   }
-
 
   const gauss = () => {
     console.log("gauss");
@@ -251,29 +257,28 @@ function App() {
     const cols = matrix[0].length;
     const detA = detMatrix(matrix);
 
-    if (detA === 0) {
-      setFound(true)
-      setAnswer("NO SOLUTION!")
-        console.log(["No unique solution exists."]);
+    if (detA === 0 || rows != cols -2) {
+      setFound(true);
+      setAnswer(["NO SOLUTION!"]);
+      console.log(["No unique solution exists."]);
+      
     }
-
-    const solutions = [];
-
-    for (let i = 0; i < cols; i++) {
+    else{
+      const solutions = [];
+      for (let i = 0; i < cols; i++) {
         const mat = src_matrix.map(row => row.slice());
-       
-        
         for (let j = 0; j < rows; j++) {
-            mat[j][i] = b[j];
+          mat[j][i] = b[j];
         }
-         console.log(mat);
+        console.log(mat);
 
         const detMat = detMatrix(mat);
         solutions.push((detMat / detA).toFixed(2));
+      }
+      setFound(true);
+      setAnswer(solutions);
+      console.log(solutions);
     }
-    setFound(true)
-    setAnswer(solutions);
-    console.log(solutions);
   }
   const inverse = () => {
     console.log("INVERSE");
